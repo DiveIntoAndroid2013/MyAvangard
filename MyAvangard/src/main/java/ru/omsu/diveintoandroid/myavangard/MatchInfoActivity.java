94ea0abddec5612079ea60144a06801fe3ffea29
Alex Korovyansky <korovyansk@gmail.com>
2013-08-09 23:01:01 +0700

    add MatchInfoActivity, add handlers for buttons in MatchActivity

---
+++ MyAvangard/src/main/java/ru/omsu/diveintoandroid/myavangard/MatchInfoActivity.java
@@ -0,0 +1,16 @@
+package ru.omsu.diveintoandroid.myavangard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * MatchInfoActivity
+ *
+ * @author Alex Korovyansky <korovyansk@gmail.com>
+ */
+public class MatchInfoActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_info);
+    }
+}
--- MyAvangard/src/main/java/ru/omsu/diveintoandroid/myavangard/MatchesActivity.java
+++ MyAvangard/src/main/java/ru/omsu/diveintoandroid/myavangard/MatchesActivity.java
@@ -1,8 +1,11 @@
 package ru.omsu.diveintoandroid.myavangard;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MatchesActivity extends Activity {
 
@@ -10,12 +13,32 @@ public class MatchesActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matches);
+        findViewById(R.id.match1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MatchesActivity.this, MatchInfoActivity.class));
+            }
+        });
+        findViewById(R.id.match2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MatchesActivity.this, MatchInfoActivity.class));
+            }
+        });
+        findViewById(R.id.site).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String url = "http://hawk.ru";
+                final Intent openBrowserIntent = new Intent(Intent.ACTION_VIEW);
+                openBrowserIntent.setData(Uri.parse(url));
+                startActivity(openBrowserIntent);
+            }
+        });
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
