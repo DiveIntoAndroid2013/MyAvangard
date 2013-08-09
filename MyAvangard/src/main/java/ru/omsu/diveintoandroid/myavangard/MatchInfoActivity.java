38f7b54fac90a050db3c786d749e6f032d486d95
Alex Korovyansky <korovyansk@gmail.com>
2013-08-09 23:26:52 +0700

    add life cycle logs

--- MyAvangard/src/main/java/ru/omsu/diveintoandroid/myavangard/MatchInfoActivity.java
+++ MyAvangard/src/main/java/ru/omsu/diveintoandroid/myavangard/MatchInfoActivity.java
@@ -2,6 +2,7 @@ package ru.omsu.diveintoandroid.myavangard;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 /**
@@ -12,6 +13,7 @@ import android.widget.TextView;
 public class MatchInfoActivity extends Activity {
 
     public static final String INTENT_KEY_MATCH_ID = "matchId";
+    private static final String TAG = MatchInfoActivity.class.getSimpleName();
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +21,40 @@ public class MatchInfoActivity extends Activity {
         final int matchId = getIntent().getIntExtra(INTENT_KEY_MATCH_ID, -1);
         final TextView matchInfoText = (TextView) findViewById(R.id.match_info_text);
         matchInfoText.setText("Info about Match " + matchId + "!");
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        Log.v(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "onDestroy");
     }
 }
--- MyAvangard/src/main/java/ru/omsu/diveintoandroid/myavangard/MatchesActivity.java
+++ MyAvangard/src/main/java/ru/omsu/diveintoandroid/myavangard/MatchesActivity.java
@@ -4,14 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
 public class MatchesActivity extends Activity {
 
+    private static final String TAG = MatchesActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v(TAG, "onCreate");
         setContentView(R.layout.activity_matches);
         findViewById(R.id.match1).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,11 +44,46 @@ public class MatchesActivity extends Activity {
         });
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "onDestroy");
     }
     
 }
